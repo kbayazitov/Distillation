8f8b84ac8424fb8b9c7f361ee8a1e74c5265657e
Kamil <90562445+kbayazitov@users.noreply.github.com>
2022-04-16 16:11:47 +0300

    Add files via upload

--- src/mylib/train.py
+++ src/mylib/train.py
@@ -27,7 +27,7 @@ def FashionDigit(mnist_dataset, fashion_targets):
         ind = np.random.choice(mnist_indices[target])
         res.append(mnist_dataset[ind][0])
     tensor_res = torch.cat(res).view([len(res), 28, 28])
-    
+
     return tensor_res.view([-1, 28,28 ]).to(device)
 
 class Perceptron(torch.nn.Module):
