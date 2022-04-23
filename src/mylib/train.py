e3d74065d5d447f22e9144ebd8f663c3db284157
Kamil <90562445+kbayazitov@users.noreply.github.com>
2022-04-23 15:18:29 +0300

    Update train.py

--- src/mylib/train.py
+++ src/mylib/train.py
@@ -71,13 +71,13 @@ class Perceptron1(torch.nn.Module):
         
         self.layers = torch.nn.Sequential()
         
-        self.layers.add_module('layer{0}', torch.nn.Linear(input_dim, 500)
+        self.layers.add_module('layer{0}', torch.nn.Linear(input_dim, 500))
         self.layers.add_module('relu{0}', torch.nn.ReLU())
-        self.layers.add_module('layer{1}', torch.nn.Linear(500, 64)
+        self.layers.add_module('layer{1}', torch.nn.Linear(500, 64))
         self.layers.add_module('relu{1}', torch.nn.ReLU())
-        self.layers.add_module('layer{2}', torch.nn.Linear(64, 64)
+        self.layers.add_module('layer{2}', torch.nn.Linear(64, 64))
         self.layers.add_module('relu{2}', torch.nn.ReLU())
-        self.layers.add_module('layer{3}', torch.nn.Linear(64, 64)
+        self.layers.add_module('layer{3}', torch.nn.Linear(64, 64))
         self.layers.add_module('relu{3}', torch.nn.ReLU())
         
         self.layers.add_module('classifier', 
