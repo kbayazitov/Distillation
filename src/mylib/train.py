7aaf4000f75093f428a13449f1cfc93228f440ec
Kamil <90562445+kbayazitov@users.noreply.github.com>
2022-06-13 16:49:07 +0500

    Update train.py

--- src/mylib/train.py
+++ src/mylib/train.py
@@ -167,7 +167,7 @@ class CNN_big(torch.nn.Module):
     def forward(self, input):
         return self.layers(input)
 
-def train_teacher(teacher, train_data, test_data, input_shape=[-1,784], epochs=10, SEED=1234, phi=lambda x: x):
+def train_teacher(teacher, train_data, test_data, input_shape=[-1,784], epochs=10, SEED=42, phi=lambda x: x):
     """
     Function for training the teacher model for the classification task
     Args:
@@ -251,7 +251,7 @@ def train_teacher_reg(teacher, train_data, test_data, SEED=1234, phi=lambda x: x
             predict = teacher(phi(x))
             loss = loss_function(predict, y)
 
-def distillation_train(student, train_data, test_data, input_shape=[-1,784], batch_size=100, epochs=20, SEED=1234, teacher=None, T=1, phi=lambda x: x):   
+def distillation_train(student, train_data, test_data, input_shape=[-1,784], batch_size=100, epochs=20, SEED=42, teacher=None, T=1, phi=lambda x: x):   
     """
     Function for training the student model for the classification task
     Args:
@@ -275,7 +275,7 @@ def distillation_train(student, train_data, test_data, input_shape=[-1,784], bat
     list_of_test_losses = []
 
     epochs = epochs
-    attempts = 1
+    attempts = 3
     
     for attempt in tqdm(range(attempts)):
         #student = Student
