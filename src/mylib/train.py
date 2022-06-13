15fad4daf372528206b4d3142dbb6b5573a78699
Kamil <90562445+kbayazitov@users.noreply.github.com>
2022-06-13 12:49:13 +0300

    Update train.py

--- src/mylib/train.py
+++ src/mylib/train.py
@@ -416,7 +416,7 @@ def generate_regression():
     """
     SEED = 1234
     rs = np.random.RandomState(SEED)
-    reg_data = rs.randn(60000, 100)
+    reg_data = rs.randn(20000, 100)
     Y = reg_data[:, 0]
     X = reg_data[:, 1:]
     X = np.hstack([np.ones((reg_data.shape[0], 1)), X])
